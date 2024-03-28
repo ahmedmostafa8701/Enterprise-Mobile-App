350cb20779a156dcd78a72d8acbe34d987ce2fb7
MarioMedhat10 <mariomedhat4g@gmail.com>
2024-03-28 15:22:20 +0200

    added sqflite with main LocalDb functions

---
+++ lib/sqflite/sqflite.dart
@@ -0,0 +1,77 @@
+import 'dart:async';
+import 'dart:developer';
+
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class LocalDb {
+
+  static Database? _db;
+
+  Future<Database?> get db async {
+    if (_db == null){
+      _db = await initialDb();
+      return _db;
+    } else {
+      return _db;
+    }
+  }
+
+  initialDb() async {
+    String databasePath = await getDatabasesPath();
+    String path = join(databasePath, "enterpriseDatabase.db");
+    Database enterpriseDb = await openDatabase(path, onCreate: _onCreate, version: 1, onUpgrade: _onUpgrade);
+    return enterpriseDb;
+  }
+
+  _onCreate(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE "accounts" (
+        "studId" TEXT NOT NULL PRIMARY KEY,
+        "email" TEXT NOT NULL,
+        "name" TEXT NOT NULL,
+        "gender" TEXT,
+        "level" TEXT,
+        "password" TEXT NOT NULL,
+        "image" TEXT NOT NULL
+      )
+    ''');
+
+    log("=================== Database Created ===================");
+  }
+
+  _onUpgrade(Database db, int oldVersion, int newVersion) {
+    log("=================== Database Upgraded ===================");
+  }
+
+  getData(String sql) async {
+    Database? appDb = await db;
+    List<Map> response = await appDb!.rawQuery(sql);
+    return response;
+  }
+
+  insertData(String sql) async {
+    Database? appDb = await db;
+    int response = await appDb!.rawInsert(sql);
+    return response;
+  }
+
+  updateData(String sql) async {
+    Database? appDb = await db;
+    int response = await appDb!.rawUpdate(sql);
+    return response;
+  }
+
+  deleteRaw(String sql) async {
+    Database? appDb = await db;
+    int response = await appDb!.rawDelete(sql);
+    return response;
+  }
+
+  deleteAllData() async {
+    String databasePath = await getDatabasesPath();
+    String path = join(databasePath, "enterpriseDatabase.db");
+    await deleteDatabase(path);
+  }
+
+}
