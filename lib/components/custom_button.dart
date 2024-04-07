d4f3a902f5cc81356437aa8eb3bd15b97e85e69b
MarioMedhat10 <mariomedhat4g@gmail.com>
2024-04-07 16:51:03 +0200

    added background, and textColor

--- lib/components/custom_button.dart
+++ lib/components/custom_button.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key, required this.text, this.onTap});
+  const CustomButton(
+      {super.key,
+      required this.text,
+      this.onTap,
+      this.background = Colors.white,
+      this.textColor = Colors.black});
 
   final String text;
   final VoidCallback? onTap;
+  final Color? background;
+  final Color? textColor;
 
   @override
   Widget build(BuildContext context) {
@@ -12,13 +19,16 @@ class CustomButton extends StatelessWidget {
       onTap: onTap,
       child: Container(
         decoration: BoxDecoration(
-          color: Colors.white,
+          color: background,
           borderRadius: BorderRadius.circular(10),
         ),
         width: double.infinity,
         height: 60,
         child: Center(
-          child: Text(text),
+          child: Text(
+            text,
+            style: TextStyle(color: textColor),
+          ),
         ),
       ),
     );
